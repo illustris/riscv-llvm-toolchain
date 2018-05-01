61872d91b7ded36a8cc791dbf368dd14fb7adcbd
illustris <rharikrishnan95@gmail.com>
2018-05-01 12:10:37 +0530

    Implemented functional hash and validate functions

--- test/debug.c
+++ test/debug.c
@@ -1,18 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define IV 0xdeadbeef1ee7c0d3;
+
 __int128 craft(unsigned int ptr, unsigned int base, unsigned int bound, unsigned int id);
+
+unsigned long long PRNG = IV;
+
+unsigned long long random64()
+{
+		PRNG = PRNG ^ (PRNG >> 1) + (PRNG >> 17);
+		return PRNG;
+}
+
+unsigned int hash_fn(unsigned long long i64)
+{
+	unsigned int ret = (unsigned int)i64;
+	ret = ret ^ (unsigned int)(i64 >> 32);
+	//printf("Hash function called\ncook = %llx\nhash = %x\n",i64,ret);
+	return ret;
+}
+
+unsigned int hash_fn_debug(unsigned long long i64)
+{
+	unsigned int ret = (unsigned int)i64;
+	ret = ret ^ (unsigned int)(i64 >> 32);
+	//printf("Hash function called\ncook = %llx\nhash = %x\n",i64,ret);
+	return ret;
+}
 
 unsigned long long hash(unsigned long long *cook_ptr)
 {
-        *cook_ptr = 0xdeadbeef1ee7c0d3;
-        return 0xbeefdead;
-		//return 0;
+	//printf("\n--------\nHASH 0x%llx\n--------\n",(unsigned long long)cook_ptr);
+    *cook_ptr = random64();
+	//printf("Setting cookie %llx at address %llx\nhash = %x\n",*cook_ptr,(unsigned long long int)cook_ptr,hash_fn_debug(*cook_ptr));
+	return hash_fn(*cook_ptr);
+	//return 0;
 }
 
 void val(unsigned long long hi, unsigned long long lo)
 {
-	//printf("\n--------\nVAL 0x%llx%llx\n--------\n",hi,lo);
+	//printf("\n--------\nVAL 0x%016llx%llx\n--------\n",hi,lo);
+	//printf("BOUND:BASE  %016llx\n", (unsigned long long int) (hi));
+    //printf("IDHASH:PTR  %016llx\n", (unsigned long long int) lo);
+	unsigned int hash = ((lo & 0xffffffff00000000) >> 32);
+	unsigned long long *base = (unsigned long long*) ((hi) & 0x00000000ffffffff);
+	if(hash != hash_fn(*base))
+	{
+		printf("!!!!VALIDATE ERROR\ngot hash %x from %08llx\nexpected %x",hash,(unsigned long long)base,hash_fn_debug(*base));
+		exit(0);
+	}
 	return;
 }
 
